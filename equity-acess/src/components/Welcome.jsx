dad7ec68e1a4034acf8743deaec7bacab625b75b
EGamert <eashan.bmvb@gmail.com>
2024-02-01 15:59:51 +0530

    talked to krishna, lol

--- equity-acess/src/components/Welcome.jsx
+++ equity-acess/src/components/Welcome.jsx
@@ -1,10 +1,26 @@
 import './Welcome.css'
 
+import { Link } from 'react-router-dom';
+
 const Welcome = ()=>{
     return(
         <>
             <div className="container">
                 <div className="image">    
+                </div>
+
+                <center><h1><b>EQUITY AND ACCESS FOR ALL</b></h1></center>
+
+                <Link to='/signup'>
+                    <button className='signup'>
+                        GET STARTED
+                    </button>
+                </Link>
+
+                <div className="options">
+
+                    
+
                 </div>
             </div>
         </>
